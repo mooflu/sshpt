758c1ba12b613a82076ce57c1d062c0dd186f147
riskable <riskable@b9f91956-fe36-11dd-85c2-bb0660c75f22>
2009-02-20 02:18:13 +0000

    It helps if you test things before you upload them =)...  Fixed setup.py so that it actually works now (to install sshpt as a module)
    git-svn-id: http://sshpt.googlecode.com/svn/trunk@11 b9f91956-fe36-11dd-85c2-bb0660c75f22

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0.4'
 
 setup(name = 'sshpt',
       license = 'GPLv3',
       version = '1.0.3',
       description = 'SSH Power Tool - Run commands and copy files to multiple servers simultaneously WITHOUT pre-shared keys',
+      classifiers=[
+	"Development Status :: 5 - Production/Stable",
+	"License :: OSI Approved :: GNU General Public License (GPL)",
+	"Operating System :: Unix",
+	"Environment :: Console",
+	"Programming Language :: Python :: 2.5",
+	"Topic :: System :: Systems Administration",
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='ssh administration parallel',
       author = 'Dan McDougall',
       author_email = 'YouKnowWho@YouKnowWhat.com',
-      py_modules = ['sshpt'],
-      scripts = ['sshpt'],
-      packages = ['paramiko'],
+      url = 'http://code.google.com/p/sshpt/',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      install_requires=[
+        "paramiko>=1.7.0",
+      ],
       )
